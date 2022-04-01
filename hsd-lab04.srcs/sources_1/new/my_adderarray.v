09c3ada9c3eb5c3b07387d34124c692cfb6417bb
ralphnation <ofbyforralph@gmail.com>
2022-04-01 22:07:44 +0900

    fix my_adderarray

--- hsd-lab04.srcs/sources_1/new/my_adderarray.v
+++ hsd-lab04.srcs/sources_1/new/my_adderarray.v
@@ -7,22 +7,18 @@ module adder_array (cmd, ain0, ain1, ain2, ain3, bin0, bin1, bin2, bin3, dout0, 
     output [3:0] overflow;
     genvar i;
     
-    wire [3:0] ain, bin, dout;
+    wire [31:0] ain[3:0], bin[3:0], dout[3:0];
+    wire ovf[3:0];
     assign {ain[0], ain[1], ain[2], ain[3]} = {ain0, ain1, ain2, ain3};
     assign {bin[0], bin[1], bin[2], bin[3]} = {bin0, bin1, bin2, bin3};
-    assign {dout[0], dout[1], dout[2], dout[3]} = {dout0, dout1, dout2, dout3};
 
     generate for (i=0; i<4; i=i+1) begin : adder
-        my_add ma (ain[i],bin[i],dout[i],overflow[i]);
+            my_add ma (ain[i],bin[i],dout[i],ovf[i]);
     end endgenerate
 
-    always @ (cmd)
-        case (cmd)
-            3'b000: begin dout0 = dout[0]; dout1 = 0; dout2 = 0; dout3 = 0; overflow[1] = 0; overflow[2] = 0; overflow[3] = 0; end
-            3'b001: begin dout0 = 0; dout1 = dout[1]; dout2 = 0; dout3 = 0; overflow[0] = 0; overflow[2] = 0; overflow[3] = 0; end
-            3'b010: begin dout0 = 0; dout1 = 0; dout2 = dout[2]; dout3 = 0; overflow[0] = 0; overflow[1] = 0; overflow[3] = 0; end
-            3'b011: begin dout0 = 0; dout1 = 0; dout2 = 0; dout3 = dout[3]; overflow[0] = 0; overflow[1] = 0; overflow[2] = 0; end
-            3'b100: begin dout0 = dout[0]; dout1 = dout[1]; dout2 = dout[2]; dout3 = dout[3]; end
-        endcase
+    assign {overflow[0], dout0} = (cmd == 0 || cmd == 4) ? {ovf[0], dout[0]} : 0;
+    assign {overflow[1], dout1} = (cmd == 1 || cmd == 4) ? {ovf[1], dout[1]} : 0;
+    assign {overflow[2], dout2} = (cmd == 2 || cmd == 4) ? {ovf[2], dout[2]} : 0;
+    assign {overflow[3], dout3} = (cmd == 3 || cmd == 4) ? {ovf[3], dout[3]} : 0;
 
 endmodule
